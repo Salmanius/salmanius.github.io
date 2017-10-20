ad9f957a7c40140c67f9849089ffb72b907eb219
Chris Cochran <ccochran-LT@ccochran-LT.local>
2017-10-19 18:13:51 -0600

    Started work on main page. Added monkey test page.

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-apiKey = "8e6439e9d46a9929d427b0483328be10";
+var apiKey = "8e6439e9d46a9929d427b0483328be10";
 
-apiURL = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + apiKey + "&per_page=1&format=json&nojsoncallback=1extras=url_o";
+var apiURL = "https://api.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=" + apiKey + "&per_page=1&format=json&nojsoncallback=1extras=url_o";
 
-function changeTitle() {
-    document.getElementById("header").innerHTML = "I changed the header.";
+function validatePage() {
+    document.getElementById("header").innerHTML = "It works. You didn't break your JavaScript.";
 }
 
-function fetchPhoto(){
+function fetchPhoto() {
     $.getJSON(apiURL, function(response){
         var ID = response.photos.photo[0].id;
         document.getElementById("photoID").innerHTML = ID;
         var newURL = "https://api.flickr.com/services/rest/?method=flickr.photos.getSizes&api_key=" + apiKey + "&photo_id=" + ID + "&per_page=1&format=json&nojsoncallback=1extras=url_o";
-        var url2 = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + apiKey" + 8e6439e9d46a9929d427b0483328be10&user_id=me&format=json&nojsoncallback=1&text=cats&extras=url_o";
+        var url2 = "https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + apiKey + "8e6439e9d46a9929d427b0483328be10&user_id=me&format=json&nojsoncallback=1&text=cats&extras=url_o";
         $.getJSON(newURL, function(response){
             var sizes = response.sizes.size;
-            alert(JSON.stri)
+            //alert(JSON.stringify(response));
             for (var i = sizes.length - 1; i >= 0; i--) {
                 if (sizes[i].label === "Original"){
                     var url = sizes[i].source;
